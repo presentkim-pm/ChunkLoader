87e2c06fbe8ecbb8b2b0649480545a329f284153
PresentKim <qjrmckd@gmail.com>
2018-05-08 23:06:35 +0900

    ListSubcommand : Show list of registered chunks instread of level's chunks

--- src/blugin/chunkloader/command/subcommands/ListSubcommand.php
+++ src/blugin/chunkloader/command/subcommands/ListSubcommand.php
@@ -8,7 +8,7 @@ use pocketmine\{
   Server, Player
 };
 use pocketmine\command\CommandSender;
-use pocketmine\level\format\Chunk;
+use pocketmine\level\Level;
 use blugin\chunkloader\command\{
   PoolCommand, SubCommand
 };
@@ -37,8 +37,8 @@ class ListSubcommand extends SubCommand{
         } else {
             return false;
         }
-        /** @var Chunk[] $list */
-        $list = array_values($level->getChunks());
+        /** @var string[] $list */
+        $list = $this->plugin->getConfig()->get($worldName = $level->getFolderName(), []);
         $max = ceil(count($list) / 5);
         $page = 0;
         if (isset($args[1]) && is_numeric($args[1])) {
@@ -49,9 +49,10 @@ class ListSubcommand extends SubCommand{
                 $page = $max;
             }
         }
-        $sender->sendMessage($this->translate('head', $level->getFolderName(), (string) ($page + 1), (string) $max));
+        $sender->sendMessage($this->translate('head', $worldName, (string) ($page + 1), (string) $max));
         for ($i = $page * 5, $count = count($list), $loopMax = ($page + 1) * 5; $i < $count && $i < $loopMax; $i++) {
-            $sender->sendMessage($this->translate('item', (string) $list[$i]->getX(), (string) $list[$i]->getZ()));
+            Level::getXZ((int) $list[$i], $chunkX, $chunkZ);
+            $sender->sendMessage($this->translate('item', (string) $chunkX, (string) $chunkZ));
         }
         return true;
     }
