b261e74f0e6031820348b546e756d9ce76141a26
PresentKim <qjrmckd@gmail.com>
2018-06-27 21:10:14 +0900

    PluginLang : Remove getArray() method, unused

--- src/kim/present/chunkloader/lang/PluginLang.php
+++ src/kim/present/chunkloader/lang/PluginLang.php
@@ -9,25 +9,6 @@ use pocketmine\lang\BaseLang;
 
 class PluginLang extends BaseLang{
 	/**
-	 * @Override for support the array at lang file
-	 *
-	 * @param string $path
-	 * @param array  $d
-	 *
-	 * @return bool
-	 */
-	protected static function loadLang(string $path, array &$d) : bool{
-		if(file_exists($path)){
-			$d = array_map(function($entry){
-				return is_string($entry) ? stripcslashes($entry) : $entry;
-			}, parse_ini_file($path, false, INI_SCANNER_RAW));
-			return true;
-		}else{
-			return false;
-		}
-	}
-
-	/**
 	 * @var ChunkLoader
 	 */
 	private $plugin;
@@ -37,7 +18,7 @@ class PluginLang extends BaseLang{
 	 * PluginLang constructor.
 	 *
 	 * @param ChunkLoader $plugin
-	 * @param string     $lang
+	 * @param string      $lang
 	 */
 	public function __construct(ChunkLoader $plugin, string $lang){
 		$this->langName = strtolower($lang);
@@ -81,24 +62,5 @@ class PluginLang extends BaseLang{
 	 */
 	public function isAvailableLanguage(string $lang) : bool{
 		return in_array(strtolower($lang), $this->getAvailableLanguageList());
-	}
-
-	/**
-	 * @param string $id
-	 *
-	 * @return string[]|null
-	 */
-	public function getArray(string $id) : ?array{
-		$result = null;
-		if(isset($this->lang[$id])){
-			$result = $this->lang[$id];
-		}elseif(isset($this->fallbackLang[$id])){
-			$result = $this->fallbackLang[$id];
-		}
-
-		if(!is_array($result)){
-			return null;
-		}
-		return $result;
 	}
 }
