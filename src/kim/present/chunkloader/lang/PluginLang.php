c03ee329652a0e78be986e775042e083f389e9e2
PresentKim <qjrmckd@gmail.com>
2018-06-29 12:14:33 +0900

    Add HEAD PHP DOC

--- src/kim/present/chunkloader/lang/PluginLang.php
+++ src/kim/present/chunkloader/lang/PluginLang.php
@@ -1,5 +1,27 @@
 <?php
 
+/*
+ *
+ *  ____                           _   _  ___
+ * |  _ \ _ __ ___  ___  ___ _ __ | |_| |/ (_)_ __ ___
+ * | |_) | '__/ _ \/ __|/ _ \ '_ \| __| ' /| | '_ ` _ \
+ * |  __/| | |  __/\__ \  __/ | | | |_| . \| | | | | | |
+ * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author  PresentKim (debe3721@gmail.com)
+ * @link    https://github.com/PresentKim
+ * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0.0
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ */
+
 declare(strict_types=1);
 
 namespace kim\present\chunkloader\lang;
