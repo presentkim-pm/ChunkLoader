4de43e0c7b97a7c6260a35c47cb0219b64ab8737
PresentKim <qjrmckd@gmail.com>
2018-07-04 02:50:43 +0900

    CheckUpdateAsyncTask : Use ETag for prevent "API rate limit exceeded"
    https://developer.github.com/v3/#rate-limiting

--- src/kim/present/chunkloader/task/CheckUpdateAsyncTask.php
+++ src/kim/present/chunkloader/task/CheckUpdateAsyncTask.php
@@ -31,6 +31,10 @@ use pocketmine\scheduler\AsyncTask;
 use pocketmine\Server;
 
 class CheckUpdateAsyncTask extends AsyncTask{
+	private const CACHE_ENTITY_TAG = "e";
+	private const CACHE_LATEST_VERSION = "v";
+	private const CACHE_FILE_NAME = "f";
+	private const CACHE_DOWNLOAD_URL = "d";
 	private const RELEASE_URL = "https://api.github.com/repos/PresentKim/ChunkLoader-PMMP/releases/latest";
 
 	/**
@@ -44,6 +48,15 @@ class CheckUpdateAsyncTask extends AsyncTask{
 	private $fileName, $downloadURL;
 
 	/**
+	 * @var string Path of latest response cache file
+	 */
+	private $cachePath;
+
+	public function __construct(){
+		$this->cachePath = ChunkLoader::getInstance()->getDataFolder() . ".latestCache";
+	}
+
+	/**
 	 * Actions to execute when run
 	 *
 	 * Get latest version for comparing with plugin version, Store to $latestVersion
@@ -52,24 +65,43 @@ class CheckUpdateAsyncTask extends AsyncTask{
 	public function onRun() : void{
 		curl_setopt_array($curlHandle = curl_init(), [
 			CURLOPT_URL => self::RELEASE_URL,
+			CURLOPT_HEADER => true,
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_SSL_VERIFYHOST => false,
 			CURLOPT_SSL_VERIFYPEER => false,
 			CURLOPT_USERAGENT => "true"
 		]);
+		$latestCache = [];
+		if(file_exists($this->cachePath)){
+			$latestCache = json_decode(file_get_contents($this->cachePath), true);
+			curl_setopt($curlHandle, CURLOPT_HTTPHEADER, ["If-None-Match: " . $latestCache[self::CACHE_ENTITY_TAG]]);
+		}
 		$response = curl_exec($curlHandle);
+		$headerSize = curl_getinfo($curlHandle, CURLINFO_HEADER_SIZE);
 		curl_close($curlHandle);
 
-		if(!empty($response)){
-			$jsonData = json_decode($response, true);
-			$this->latestVersion = $jsonData["tag_name"];
+		$header = substr($response, 0, $headerSize);
+		if(!strpos($header, "304 Not Modified")){
+			foreach(explode(PHP_EOL, $header) as $key => $line){
+				if(strpos($line, "ETag: ") === 0){ //starts with "ETag: "
+					$latestCache[self::CACHE_ENTITY_TAG] = substr($line, strlen("ETag: "));
+				}
+			}
+			$jsonData = json_decode(substr($response, $headerSize), true);
+			$latestCache[self::CACHE_LATEST_VERSION] = $jsonData["tag_name"];
 			foreach($jsonData["assets"] as $key => $assetData){
 				if(substr_compare($assetData["name"], ".phar", -strlen(".phar")) === 0){ //ends with ".phar"
-					$this->fileName = $assetData["name"];
-					$this->downloadURL = $assetData["browser_download_url"];
+					$latestCache[self::CACHE_FILE_NAME] = $assetData["name"];
+					$latestCache[self::CACHE_DOWNLOAD_URL] = $assetData["browser_download_url"];
 				}
 			}
 		}
+
+		file_put_contents($this->cachePath, json_encode($latestCache));
+
+		$this->latestVersion = $latestCache[self::CACHE_LATEST_VERSION];
+		$this->fileName = $latestCache[self::CACHE_FILE_NAME];
+		$this->downloadURL = $latestCache[self::CACHE_DOWNLOAD_URL];
 	}
 
 	/**
