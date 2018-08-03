797e91658b245159b7df75e7ada00a5a686d7fee
PresentKim <qjrmckd@gmail.com>
2018-08-04 03:18:00 +0900

    PluginLang : Rename getAvailableLanguageList() to getLanguageList

--- src/kim/present/chunkloader/lang/PluginLang.php
+++ src/kim/present/chunkloader/lang/PluginLang.php
@@ -116,7 +116,7 @@ class PluginLang{
 	 *
 	 * @return string[]
 	 */
-	public function getAvailableLanguageList() : array{
+	public function getLanguageList() : array{
 		return explode("\n", file_get_contents("{$this->plugin->getDataFolder()}lang/language.list"));
 	}
 
@@ -126,6 +126,6 @@ class PluginLang{
 	 * @return bool
 	 */
 	public function isAvailableLanguage(string $lang) : bool{
-		return in_array(strtolower($lang), $this->getAvailableLanguageList());
+		return in_array(strtolower($lang), $this->getLanguageList());
 	}
 }
