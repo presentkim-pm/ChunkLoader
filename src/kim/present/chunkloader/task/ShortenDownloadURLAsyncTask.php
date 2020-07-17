45d5ca1c4045adbf53db54867577de0494e782ce
PresentKim <debe3721@gmail.com>
2020-07-17 12:09:51 +0900

    Change license to LGPL-3.0

--- src/kim/present/chunkloader/task/ShortenDownloadURLAsyncTask.php
+++ src/kim/present/chunkloader/task/ShortenDownloadURLAsyncTask.php
@@ -7,13 +7,16 @@
  * | |_) | '__/ _ \/ __|/ _ \ '_ \| __| ' /| | '_ ` _ \
  * |  __/| | |  __/\__ \  __/ | | | |_| . \| | | | | | |
  * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
+ *                |___/
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the MIT License. see <https://opensource.org/licenses/MIT>.
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * @author  PresentKim (debe3721@gmail.com)
  * @link    https://github.com/PresentKim
- * @license https://opensource.org/licenses/MIT MIT License
+ * @license https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
  *
  *   (\ /)
  *  ( . .) ♥
