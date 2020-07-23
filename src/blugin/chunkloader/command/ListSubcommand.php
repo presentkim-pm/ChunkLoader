f536afde7c171b3e276da226fd4f94f64d0e9921
PresentKim <debe3721@gmail.com>
2020-07-23 23:34:53 +0900

    ListCommand : Prevent show empty list

--- src/blugin/chunkloader/command/ListSubcommand.php
+++ src/blugin/chunkloader/command/ListSubcommand.php
@@ -49,15 +49,19 @@ class ListSubcommand extends Subcommand{
      */
     public function execute(CommandSender $sender, array $args = []) : bool{
         $world = $this->getWorld($sender, array_shift($args));
-        $worldName = $world->getFolderName();
         /** @var ChunkLoader $plugin */
         $plugin = $this->getMainCommand()->getOwningPlugin();
         $chunkHashs = $plugin->getByWorld($world);
+        if(empty($chunkHashs)){
+            $this->sendMessage($sender, "failure.empty", [$world->getFolderName()]);
+            return true;
+        }
+
         $list = array_chunk($chunkHashs, $sender->getScreenLineHeight());
         $page = NumberArgumentValidator::validateRange(array_shift($args) ?? "1", 1, count($list));
 
         //Send list of registered chunk
-        $this->sendMessage($sender, "head", [$worldName, $page, count($list)]);
+        $this->sendMessage($sender, "head", [$world->getFolderName(), $page, count($list)]);
         if(isset($list[$page - 1])){
             foreach($list[$page - 1] as $chunkHash){
                 World::getXZ($chunkHash, $chunkX, $chunkZ);
